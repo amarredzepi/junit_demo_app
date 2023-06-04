a39ecb6b2a1d0d6f970dbf985236cac23b65fa06
amarredzepi <amar-redzepi@hotmail.com>
2023-06-04 20:50:04 +0200

    Commit der Tests

---
+++ src/test/java/com/example/demo/junitdemoapp/JunitdemoappApplicationTests.java
@@ -0,0 +1,9 @@
+package com.example.demo.junitdemoapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitdemoappApplicationTests {
+//Basis DemoAppTest Klasse
+
+}
